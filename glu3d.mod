5939f0c5369bef21d62cabb2b035e7fe636a5d11
Michael Hines <michael.hines@yale.edu>
2019-08-16 15:52:10 -0400

    ampa nmda receptors

--- glu3d.mod
+++ glu3d.mod
@@ -1,8 +1,9 @@
 NEURON {
   POINT_PROCESS Glu3d
   RANGE vesicle_src, uncage_src
-  RANGE up_rate, upglu_rate, up0
-  GLOBAL nx, ny, nz
+  RANGE up_rate, upglu_rate, up0, kampa, knmda, Kdampa, Kdnmda
+  RANGE ijampa, ijnmda
+  GLOBAL nx, ny, nz, nampa, nnmda
 }
 
 DEFINE NX 35
@@ -10,10 +11,14 @@ DEFINE NY 35
 DEFINE NZ 3
 DEFINE NXY 1225 : manually set to NX*NY
 DEFINE NXYZ 3675 : manually set to NX*NY*NZ
+DEFINE Nampa 100
+DEFINE Nnmda 100
 
 UNITS {
   (molar) = (1/liter)
   (mM) = (millimolar)
+  (uM) = (micromolar)
+  (nM) = (nanomolar)
   (um) = (micron)
   : (nm) = (nanometer) already defined
 }
@@ -32,6 +37,18 @@ PARAMETER {
   up_rate = 0 (/ms) : rate of binding of up and glu
   upglu_rate = 0 (/ms) : rate of glu unbinding into uptake
   up0 : # molecules of up per surface compartment
+
+  nampa = Nampa
+  nnmda = Nnmda
+  Kdampa = 5 :(uM)
+  kampa = 0 (/ms)
+  Kdnmda = 25 :(nM)
+  knmda = 0 (/ms)
+}
+
+ASSIGNED {
+  ijampa[Nampa]
+  ijnmda[Nnmda]
 }
 
 STATE {
@@ -40,6 +57,10 @@ STATE {
   upt[NXY] (1) : uptake molecule binding/unbinding generates  max uptake rate
   upglu[NXY] (1)
   uptake (1)
+  ampa[Nampa]
+  ampaglu[Nampa]
+  nmda[Nnmda]
+  nmdaglu[Nnmda]
 }
 
 INITIAL {
@@ -55,6 +76,7 @@ KINETIC scheme {
   COMPARTMENT dx*dx*dx {clearance}
   COMPARTMENT dx*dx*dx {uptake}
   COMPARTMENT i, dx*dx*dx {upt upglu}
+  COMPARTMENT i, dx*dx*dx {ampa ampaglu nmda nmdaglu}
 
   : Diffusion and clearance boundary condition
   c = D*dx*(1e6)
@@ -87,6 +109,15 @@ KINETIC scheme {
       ~ glu[i + j*NX] + upt[i + j*NX]<-> upglu[i + j*NX] (dx*dx*dx*up_rate, 0)
       ~ upglu[i + j*NX] <-> upt[i + j*NX] + uptake (dx*dx*dx*upglu_rate, 0)
     }
+  }
+
+  : ampa
+  FROM i = 0 TO Nampa-1 {
+    ~ ampa[i] + glu[INT(ijampa[i])] <-> ampaglu[i] (dx*dx*dx*Kdampa*kampa, dx*dx*dx*kampa)
+  }
+  :nmda
+  FROM i = 0 TO Nnmda-1 {
+    ~ nmda[i] + glu[INT(ijnmda[i])] <-> nmdaglu[i] (dx*dx*dx*Kdnmda*knmda, dx*dx*dx*knmda)
   }
 }
 
--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 from neuron import h, gui
+import math
 s = h.Section() # just a home for the Glu2D instance
 glu = h.Glu3d(s(.5))
-h.load_file("test1.ses")
 
 
 nx=int(h.nx_Glu3d)
@@ -12,11 +12,47 @@ h('ny=%d'%ny)
 h('nz=%d'%nz)
 h('''func ijk() { return $1 + nx*$2 + nx*ny*$3 }''')
 dx = h.dx_Glu3d # (nanometer)
+nampa=int(h.nampa_Glu3d)
+nnmda=int(h.nnmda_Glu3d)
 
 def molec2mM(x):
   vol = dx*dx*dx * (1e-24) # (liters)
   return x/vol/6.0221418e23 * 1000
 
+# 500 Glutamate molecules in 1/4 vesicle at 0,0,0 dump in 100us
+delay = 0.01 #ms
+dur = 0.01 #ms
+amp = 500/dur
+vsrc = h.Vector([0, 0, amp, amp, 0, 0])
+tsrc = h.Vector([0, delay, delay, delay+dur, delay+dur, 1e9])
+vsrc.play(glu._ref_vesicle_src, tsrc, 1)
+
+# place nampa receptors from center to 100 um away
+def place_receptors(n, d1, d2):
+  # all the squares lying in the annulus from d1 to d2 from the center
+  squares = []
+  for i in range(nx):
+    for j in range(ny):
+       d = math.sqrt((dx*i)**2 + (dx*j)**2)
+       if d >= d1 and d <= d2:
+          squares.append((i,j))
+  ix = int(len(squares)/n)
+  print(len(squares), n, ix)
+  locations = []
+  for i in range(n):
+    locations.append(squares[i*ix])
+  return locations
+
+locations = place_receptors(nampa, 0., 100.)
+for i in range(nampa):
+  glu.ijampa[i] = h.ijk(locations[i][0], locations[i][1], nz-1)
+
+locations = place_receptors(nnmda, 150., 250.)
+for i in range(nnmda):
+  glu.ijnmda[i] = h.ijk(*locations[i], nz-1)
+
+
+h.load_file("test1.ses")
 
 sl = h.SectionList()
 hin = h.PlotShape[0]
@@ -43,13 +79,5 @@ for i in range(nx):
 gluline2.plot(glulinegraph)
 #glulinegraph2.size(0, 30, 0, 3)
 h.flush_list.append(glulinegraph)
-
-# 500 Glutamate molecules in 1/4 vesicle at 0,0,0 dump in 100us
-delay = 0.01 #ms
-dur = 0.01 #ms
-amp = 500/dur
-vsrc = h.Vector([0, 0, amp, amp, 0, 0])
-tsrc = h.Vector([0, delay, delay, delay+dur, delay+dur, 1e9])
-vsrc.play(glu._ref_vesicle_src, tsrc, 1)
 
 h.stdinit()
